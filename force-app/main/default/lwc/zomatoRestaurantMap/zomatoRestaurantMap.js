b2e705855891b095d003c242eae423dcfa3fbbd6
LokeshSagi <lokesh.sagi@gmail.com>
2019-05-27 18:22:51 +0530

    Location modified

--- force-app/main/default/lwc/zomatoRestaurantMap/zomatoRestaurantMap.js
+++ force-app/main/default/lwc/zomatoRestaurantMap/zomatoRestaurantMap.js
@@ -1,23 +1,46 @@
+/* eslint-disable no-console */
 /* eslint-disable no-alert */
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class ZomatoRestaurantMap extends LightningElement {
     @api restaurant; 
-    mapMarks;
+    
+    @track lat;
+    @track long;
     get mapMarkers() {
         //alert('Inside -> '+JSON.stringify(this.restaurant));
-        this.mapMarks = [
+        let mapMarks = [];
+        mapMarks.push(
             {
                 location: {
                     'Latitude': this.restaurant.restaurant.location.latitude,
                     'Longitude': this.restaurant.restaurant.location.longitude
                 },
                 title: this.restaurant.restaurant.name,
+                description: this.restaurant.restaurant.location.address
 
+            },
+            {
+                location: {
+                    'Latitude': this.lat,
+                    'Longitude': this.long
+                },
+                title: 'Me',
+                icon: 'standard:user'
             }
-        ]
+        )
 
-        return this.mapMarks;
+        return mapMarks;
+    }
+
+    connectedCallback() {
+        navigator.geolocation.getCurrentPosition(success => {
+            this.lat = success.coords.latitude;
+            this.long = success.coords.longitude;
+            console.log('lat -> '+this.lat);
+            console.log('long -> '+this.long);
+            console.log('Rest lat -> '+this.restaurant.restaurant.location.latitude);
+        });
     }
 
 }
